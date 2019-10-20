8742452d0ce8cf19707a2b13eba1a79f08870099
Arman hossain <armanh688@gmail.com>
2019-10-21 00:10:37 +0600

    responsive collapse

---
+++ responsive-collapse.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="css/slick-theme.css>
+  <link rel="stylesheet" href="css/uikit.min.css">
+  <link rel="shortcut icon" type="image/x-icon" href="img/a.png">
+  <link rel="stylesheet" href="css/font-awesome.min.css">
+  <link rel="stylesheet" href="css/bootstrap.css">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/slick.css">
+	<title>responsive collapse</title>
+</head>
+<body>
+	<!-- start faq section -->
+<section id="faq" class="py-5 bg-dark">
+  <div class="container">
+    <div class="row text-center">
+      <div class="col pb-5">
+        <h2 class='text-light'>Frequently Asked Questions</h2>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-6">
+        <div class="card bg-light text-dark">
+          <div class="card-header" id="heading-one">
+            <h5 class="mb-0">
+            <a href="#collapseOne" data-toggle="collapse" class="text-dark">
+              Question One ?
+            </a>
+            </h5>
+          </div>
+          <div id="collapseOne" class='collapse p-4'  data-parent="#faq">
+            <p class="lead mb-0">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae, explicabo eaque veniam excepturi itaque provident minima! Tempora eveniet quidem, sequi!
+            </p>
+          </div>
+        </div>
+        <div class="card bg-light text-dark my-3">
+          <div class="card-header" id="heading-two">
+            <h5 class="mb-0">
+            <a href="#collapseTwo" data-toggle="collapse" class="text-dark">
+              Question Two ?
+            </a>
+            </h5>
+          </div>
+          <div id="collapseTwo" class='collapse p-4'  data-parent="#faq">
+            <p class="lead mb-0">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae, explicabo eaque veniam excepturi itaque provident minima! Tempora eveniet quidem, sequi!
+            </p>
+          </div>
+        </div>
+        <div class="card bg-light text-dark my-3">
+          <div class="card-header" id="heading-three">
+            <h5 class="mb-0">
+            <a href="#collapseThree" data-toggle="collapse" class="text-dark">
+              Question Three ?
+            </a>
+            </h5>
+          </div>
+          <div id="collapseThree" class='collapse p-4'  data-parent="#faq">
+            <p class="lead mb-0">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae, explicabo eaque veniam excepturi itaque provident minima! Tempora eveniet quidem, sequi!
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6">
+        <div class="card bg-light text-dark">
+          <div class="card-header" id="heading-four">
+            <h5 class="mb-0">
+            <a href="#collapseFour" data-toggle="collapse" class="text-dark">
+              Question Four ?
+            </a>
+            </h5>
+          </div>
+          <div id="collapseFour" class='collapse p-4'  data-parent="#faq">
+            <p class="lead mb-0">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae, explicabo eaque veniam excepturi itaque provident minima! Tempora eveniet quidem, sequi!
+            </p>
+          </div>
+        </div>
+        <div class="card bg-light text-dark my-3">
+          <div class="card-header" id="heading-five">
+            <h5 class="mb-0">
+            <a href="#collapseFive" data-toggle="collapse" class="text-dark">
+              Question Five ?
+            </a>
+            </h5>
+          </div>
+          <div id="collapseFive" class='collapse p-4'  data-parent="#faq">
+            <p class="lead mb-0">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae, explicabo eaque veniam excepturi itaque provident minima! Tempora eveniet quidem, sequi!
+            </p>
+          </div>
+        </div>
+        <div class="card bg-light text-dark">
+          <div class="card-header" id="heading-six">
+            <h5 class="mb-0">
+            <a href="#collapseSix" data-toggle="collapse" class="text-dark">
+              Question Six ?
+            </a>
+            </h5>
+          </div>
+          <div id="collapseSix" class='collapse p-4'  data-parent="#faq">
+            <p class="lead mb-0">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae, explicabo eaque veniam excepturi itaque provident minima! Tempora eveniet quidem, sequi!
+            </p>
+          </div>
+        </div>
+    </div>
+  </div>
+</section>
+<!-- end faq section -->
+ <script src="js/jquery.min.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/uikit.min.js"></script>
+  <script src="js/uikit-icons.min.js"></script>
+  <script src="js/slick.min.js"></script>
+  <script src="js/main.js"></script>
+</body>
+</html>
